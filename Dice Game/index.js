e1123185161e00717f6fee4da04ca6b8d32c2b79
Virupakshi1998 <vineethspring@gmail.com>
2025-02-13 08:31:21 -0600

    Update index.js

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -1,10 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;  //1-6, used Math.random() to first generate a random number and used Math.floor() to round of the 
-                                                        // decimal point.
-var randomDiceImage = "dice"+randomNumber1+".png";      // dice1.png - dice6.png
+                                                        // decimal point and other things like * 6 and adding a 1 is to make the range from 1 -6.
+var randomDiceImage = "dice"+randomNumber1+".png";      // dice1.png - dice6.png, to come in this format, so that we could use this in "src" section of image 1 and
+                                                        //2 in html file, which basically is used to show a respective dice number image for that respective random 
+                                                        // number picked through Math.random()
 
 var randomImageSource = "images/"+randomDiceImage;     //images/dice1.png - images/dice6.png
 
-var imageleft = document.querySelectorAll("img")[0];
+var imageleft = document.querySelectorAll("img")[0];   // 
 
 imageleft.setAttribute("src", randomImageSource);
 
